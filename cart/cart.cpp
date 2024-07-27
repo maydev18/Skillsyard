a1b8a172731859ae7d5e05eb04034e528dd016de
Mayank <ms772254@gmail.com>
2024-07-27 17:05:18 +0530

    cart completed

--- cart/cart.cpp
+++ cart/cart.cpp
@@ -45,14 +45,18 @@ class Cart{
         current++;
     }
     void printCart(){
+        if(current == 0){
+            cout << "The cart is empty , please add some items into your cart" << endl;
+            return;
+        }
+        cout << "Item Number" << " | " <<  "Item Name" << " | " << "Item Brand" << " | " << "Item Price" << " | " << "Item Quantity" << endl;
         for(int i = 0 ; i < current ; i++){
-            cout << "-------------------------------------" << endl;
-            cout << "Item number : " << i+1 << endl;
-            cout << "name : " << cart[i].name << endl;
-            cout << "brand : " << cart[i].brand << endl;
-            cout << "price : " << cart[i].price << endl;
-            cout << "quantity : " << cart[i].quantity << endl;
-            cout << "-------------------------------------" << endl;
+            cout << i+1 << " | ";
+            cout << cart[i].name << "      |      ";
+            cout << cart[i].brand << "      |      ";
+            cout << cart[i].price << "      |      ";
+            cout << cart[i].quantity << "     |     ";
+            cout << endl;
         }
     }
     void totalPrice(){
@@ -62,12 +66,57 @@ class Cart{
         }
         cout << "The total cart amount is : " << total << endl;
     }
+    void deleteCartItem(){
+        if(current == 0){
+            cout << "The cart is already empty" << endl;
+            return;
+        }
+        cout << "Enter the item number you want to delete" << endl;
+        printCart();
+        int item;
+        cin >> item;
+        item--;
+        for(int i = item+1 ; i < current ; i++){
+            cart[i-1] = cart[i];
+        }
+        current--;
+        printCart();
+    }
+
 };
 int Cart :: current = 0;
 int main(){
+    cout << "***************************" <<endl;
+    cout << "Welcome to Dmart" << endl;
+    cout << "***************************" <<endl;
     Cart c;
-    c.addCartItem();
-    c.printCart();
-    c.totalPrice();
-
+    while(true){
+        cout << "1. Add a new product in your cart" << endl;
+        cout << "2. Delete a item from your cart" << endl;
+        cout << "3. Get the total amount of the cart" << endl;
+        cout << "4. Display the Cart" << endl;
+        cout << "5. Exit" << endl;
+        int choice;
+        cout << "Please enter your choice : "; cin >> choice;
+        if(choice == 1){
+            c.addCartItem();
+        }
+        else if(choice == 2){
+            c.deleteCartItem();
+        }
+        else if(choice == 3){
+            c.totalPrice();
+        }
+        else if(choice == 4){
+            c.printCart();
+        }
+        else if(choice == 5){
+            cout << "Thankyou for visiting us, see you again" << endl;
+            break;
+        }
+        else{
+            cout << "Please select a valid choice" << endl;
+            break;
+        }
+    }
 }
